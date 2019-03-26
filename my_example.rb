3e96d6168a44269aa81de8ea1fdae56c735159ed
Andre Jesus <andre.jesus.13.94@gmail.com>
2019-03-26 09:17:17 +0000

    bubble sort fixes

--- my_example.rb
+++ my_example.rb
@@ -2,6 +2,7 @@ require File.expand_path("./lib/bubble_sort.rb",__dir__)
 
 ar = BubbleSort.new
 ord_ar = ar.bubble_sort([10, 3, 19, 2, 5, 30, 1])
+
 puts "Unordered array:"
 puts [10, 3, 19, 2, 5, 30, 1]
 puts "Ordered array:"
